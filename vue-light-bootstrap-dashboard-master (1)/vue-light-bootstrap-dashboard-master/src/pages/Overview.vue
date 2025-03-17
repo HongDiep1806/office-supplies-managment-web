8e9adb2ee317fb7d741faabf68fced3bf616d5a1
HongDiep1806 <diep.tuthi.cit21@eiu.edu.vn>
2025-03-17 23:53:12 +0700

    last update ui

--- vue-light-bootstrap-dashboard-master (1)/vue-light-bootstrap-dashboard-master/src/pages/Overview.vue
+++ vue-light-bootstrap-dashboard-master (1)/vue-light-bootstrap-dashboard-master/src/pages/Overview.vue
@@ -19,65 +19,6 @@
       </div>
 
       <div class="row" v-if="userRole === 'Sup Leader'">
-        <!-- <div class="col-xl-3 col-md-6">
-          <stats-card>
-            <template v-slot:header>
-              <i class="nc-icon nc-refresh-02 text-warning"></i>
-            </template>
-            <template v-slot:content>
-              <p class="card-category">Yêu cầu chờ duyệt</p>
-              <h4 class="card-title">105GB</h4>
-            </template>
-            <template v-slot:footer>
-              <i class="fa fa-refresh"></i> Updated now
-            </template>
-          </stats-card>
-        </div> -->
-
-        <!-- <div class="col-xl-3 col-md-6">
-          <stats-card>
-            <template v-slot:header>
-              <i class="nc-icon nc-check-2 text-success"></i>
-            </template>
-            <template v-slot:content>
-              <p class="card-category">Yêu cầu đã duyệt</p>
-              <h4 class="card-title">$1,345</h4>
-            </template>
-            <template v-slot:footer>
-              <i class="fa fa-calendar-o"></i> Last day
-            </template>
-          </stats-card>
-        </div> -->
-
-        <!-- <div class="col-xl-3 col-md-6">
-          <stats-card>
-            <template v-slot:header>
-              <i class="nc-icon nc-refresh-02 text-danger"></i>
-            </template>
-            <template v-slot:content>
-              <p class="card-category">Tổng hợp chờ duyệt</p>
-              <h4 class="card-title">45</h4>
-            </template>
-            <template v-slot:footer>
-              <i class="fa fa-clock-o"></i> Last day
-            </template>
-          </stats-card>
-        </div> -->
-
-        <!-- <div class="col-xl-3 col-md-6">
-          <stats-card>
-            <template v-slot:header>
-              <i class="nc-icon nc-check-2 text-success"></i>
-            </template>
-            <template v-slot:content>
-              <p class="card-category">Tổng hợp đã duyệt</p>
-              <h4 class="card-title">+45</h4>
-            </template>
-            <template v-slot:footer>
-              <i class="fa fa-refresh"></i> Updated now
-            </template>
-          </stats-card>
-        </div> -->
       </div>
 
       <div class="row" v-if="userRole === 'Sup Leader'">
@@ -131,6 +72,20 @@
         </div>
       </div>
 
+      <div class="row" v-if="userRole === 'Dep Leader'">
+        <div class="col-12">
+          <card class="strpied-tabled-with-hover" body-classes="table-full-width table-responsive">
+            <template v-slot:header>
+              <h4 class="card-title">Phiếu yêu cầu gần đây</h4>
+              <p class="card-category">Các Phiếu yêu cầu đã tạo gần đây của phòng {{ department }}</p>
+            </template>
+            <l-table class="table-hover table-striped" :columns="tableData.columns" :data="tableData.data"
+              :displayStatus="true" :domain="'request'" :displayActions="true" :canEdit="false" :canDelete="false"
+              :canView="true" :apiURL="'https://localhost:7162/Request'"></l-table>
+          </card>
+        </div>
+      </div>
+
 
       <div class="row" v-if="userRole === 'Sup Leader'">
         <div class="col-12">
@@ -139,20 +94,21 @@
               <h4 class="card-title">Báo cáo chi tiết Về việc Cấp phát VPP</h4>
               <p class="card-category">Các Phiếu yêu cầu đã tạo gần đây của bạn</p>
               <div class="row">
-        <div class="col-md-6">
-          <label for="start-date">Ngày bắt đầu:</label>
-          <input type="date" id="start-date" v-model="startDate" class="form-control">
-        </div>
-        <div class="col-md-6">
-          <label for="end-date">Ngày kết thúc:</label>
-          <input type="date" id="end-date" v-model="endDate" class="form-control">
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-md-12">
-          <button @click="fetchSummariesByDateRange" class="btn btn-info btn-fill float-right" style="margin-top: 5px;">Lấy Dữ Liệu</button>
-        </div>
-      </div>
+                <div class="col-md-6">
+                  <label for="start-date">Ngày bắt đầu:</label>
+                  <input type="date" id="start-date" v-model="startDate" class="form-control">
+                </div>
+                <div class="col-md-6">
+                  <label for="end-date">Ngày kết thúc:</label>
+                  <input type="date" id="end-date" v-model="endDate" class="form-control">
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-12">
+                  <button @click="fetchSummariesByDateRange" class="btn btn-info btn-fill float-right"
+                    style="margin-top: 5px;">Lấy Dữ Liệu</button>
+                </div>
+              </div>
             </template>
             <l-table class="table-hover table-striped" :columns="tableData.columns" :data="tableData.data"
               :displayStatus="false" :domain="'request'" :displayActions="true" :canEdit="false" :canDelete="false"
@@ -188,9 +144,10 @@ export default {
       department: '',
       permissions: [],
       tableData: {
-        columns: ['STT', 'Mã số phiếu', 'Ngày tạo', 'Tổng tiền'],
+        columns: ['STT', 'Mã số phiếu', 'Người tạo', 'Ngày tạo', 'Tổng tiền'],
         data: [],
       },
+      token: '',
       startDate: '', // Biến cho ngày bắt đầu
       endDate: '',   // Biến cho ngày kết thúc
       barChart: {
@@ -225,38 +182,39 @@ export default {
   },
   async mounted() {
     this.updateClock();
-  setInterval(this.updateClock, 1000);
-  
-  // Gọi API để lấy dữ liệu với endDate là ngày hiện tại
-  const datetimenow = new Date().toISOString().split('T')[0];
-  this.endDate = datetimenow; // Gán giá trị cho endDate
-  await this.fetchSummariesByDateRange(); // Gọi hàm để fetch dữ liệu
+    setInterval(this.updateClock, 1000);
 
-  const token = localStorage.getItem('authToken');
-  if (token) {
-    try {
-      const decoded = jwtDecode(token);
-      this.userName = decoded.name || 'User';
-      this.userRole = decoded.Role || 'User';
-      this.userID = decoded.sub || 'User';
-      this.permissions = decoded.Permission || [];
-      this.department = decoded.Department || 'User';
-      localStorage.setItem('userName', this.userName);
-      localStorage.setItem('userRole', this.userRole);
-      localStorage.setItem('userId', this.userID);
-      localStorage.setItem('permissions', JSON.stringify(this.permissions));
-      localStorage.setItem('department', this.department);
-    } catch (error) {
-      console.error('Invalid token:', error);
+    // Gọi API để lấy dữ liệu với endDate là ngày hiện tại
+    const datetimenow = new Date().toISOString().split('T')[0];
+    this.endDate = datetimenow; // Gán giá trị cho endDate
+    await this.fetchSummariesByDateRange(); // Gọi hàm để fetch dữ liệu
+
+    const token = localStorage.getItem('authToken');
+    this.token = token;
+    if (token) {
+      try {
+        const decoded = jwtDecode(token);
+        this.userName = decoded.name || 'User';
+        this.userRole = decoded.Role || 'User';
+        this.userID = decoded.sub || 'User';
+        this.permissions = decoded.Permission || [];
+        this.department = decoded.Department || 'User';
+        localStorage.setItem('userName', this.userName);
+        localStorage.setItem('userRole', this.userRole);
+        localStorage.setItem('userId', this.userID);
+        localStorage.setItem('permissions', JSON.stringify(this.permissions));
+        localStorage.setItem('department', this.department);
+      } catch (error) {
+        console.error('Invalid token:', error);
+      }
     }
-  }
 
-  if (this.userRole === 'Finance Management Employee' || this.userRole === 'Employee') {
-    await this.fetchRequestData();
-  }
-  if (this.userRole === 'Sup Leader') {
-    await this.fetchReportData();
-  }
+    if (this.userRole === 'Finance Management Employee' || this.userRole === 'Employee'|| this.userRole === 'Dep Leader') {
+      await this.fetchRequestData();
+    }
+    if (this.userRole === 'Sup Leader') {
+      await this.fetchReportData();
+    }
   },
   methods: {
     updateClock() {
@@ -267,14 +225,34 @@ export default {
       if (!this.userID) return;
 
       try {
-        const response = await axios.get(`https://localhost:7162/Request/${this.userID}`, { timeout: 50000 });
+        let response;
+        if (this.userRole === 'Finance Management Employee') {
+          response = await axios.get(`https://localhost:7162/Request/approved-requests-list`, {
+            headers: { Authorization: `Bearer ${this.token}` },
+            timeout: 100000
+          });
+        } else if (this.userRole === 'Employee') {
+          response = await axios.get(`https://localhost:7162/Request/${this.userID}`, { timeout: 50000 });
+        }else if(this.userRole === 'Dep Leader'){
+          response = await axios.get(`https://localhost:7162/Request/department/${this.department}`, {
+                headers: { Authorization: `Bearer ${this.token}` },
+                timeout: 100000
+            });
+        }
+
+        // Lấy danh sách các Promise từ getUserName
+        const userPromises = response.data.map(item => this.getUserName(item.userID));
+
+        // Giải quyết tất cả các Promise
+        const userNames = await Promise.all(userPromises);
 
         this.tableData.data = response.data
           .sort((a, b) => new Date(b.createdDate) - new Date(a.createdDate))
           .slice(0, 10)
-          .map((item) => ({
+          .map((item, index) => ({ // Thêm index để lấy tên người dùng tương ứng
             requestID: item.requestID,
             'Mã số phiếu': item.requestCode,
+            'Người tạo': userNames[index], // Lấy tên người dùng từ mảng đã giải quyết
             'Ngày tạo': new Date(item.createdDate).toLocaleString('vi-VN', {
               hour: '2-digit',
               minute: '2-digit',
@@ -283,10 +261,44 @@ export default {
               year: 'numeric'
             }).replace(',', ''),
             'Tổng tiền': new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(item.totalPrice),
-            Status: item.isApprovedBySupLead && item.isApprovedByDepLead ? "Đã duyệt" : item.isApprovedByDepLead && !item.isApprovedBySupLead ? "Đang xử lý" : "Chưa duyệt"
+            Status: (() => {
+                if (this.userRole === 'Dep Leader') {
+                  if (!item.isProcessedByDepLead) {
+                    return "Chưa duyệt";
+                  } else if (item.isProcessedByDepLead && item.isApprovedByDepLead) {
+                    return "Đã duyệt";
+                  } else if (item.isProcessedByDepLead && !item.isApprovedByDepLead) {
+                    return "Không duyệt";
+                  }
+                } else if (this.userRole === 'Finance Management Employee') {
+                  if (!item.isCollectedInSummary) {
+                    if (item.isApprovedByDepLead && !item.isProcessedByDepLead) {
+                      return "Không duyệt";
+                    } else if (item.isApprovedByDepLead && item.isApprovedBySupLead) {
+                      return "Đã duyệt";
+                    } else if (item.isApprovedByDepLead && !item.isApprovedBySupLead) {
+                      return "Chưa duyệt";
+                    }
+                  } else {
+                    return "Đã tổng hợp";
+                  }
+
+                }
+                return "Không xác định"; // Trường hợp mặc định
+              })(),
           }));
+          console.log("log data sau khi map", this.tableData.data);
       } catch (error) {
         console.error('Lỗi khi lấy danh sách phiếu yêu cầu:', error);
+      }
+    },
+    async getUserName(userID) {
+      try {
+        const response = await axios.get(`https://localhost:7162/User/getNameById${userID}`, { timeout: 50000 });
+        return response.data;
+      } catch (error) {
+        console.error('Lỗi khi lấy tên người dùng:', error);
+        return 'Người dùng không xác định'; // Trả về giá trị mặc định nếu có lỗi
       }
     },
     async fetchReportData() {
@@ -333,81 +345,87 @@ export default {
       }
     },
     async fetchSummariesByDateRange() {
-  const today = new Date();
-  const selectedStartDate = new Date(this.startDate);
-  const selectedEndDate = new Date(this.endDate);
+      const today = new Date();
+      const selectedStartDate = new Date(this.startDate);
+      const selectedEndDate = new Date(this.endDate);
 
-  // Kiểm tra xem ngày bắt đầu và ngày kết thúc có lớn hơn ngày hiện tại không
-  if (selectedStartDate > today || selectedEndDate > today) {
-    alert('Ngày bắt đầu và ngày kết thúc không được lớn hơn ngày hiện tại.');
-    return;
-  }
-
-  // Nếu chỉ có endDate và không có startDate
-  if (!this.startDate && this.endDate) {
-    try {
-      const response = await axios.get(`https://localhost:7162/Request/requests-in-date-range`, {
-        params: {
-          endDate: this.endDate,
-        },
-      });
-
-      if (response.data && Array.isArray(response.data)) {
-        this.tableData.data = response.data.map((item) => ({
-          requestID: item.requestID,
-          'Mã số phiếu': item.requestCode,
-          'Tổng tiền': new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(item.totalPrice),
-          'Ngày tạo': new Date(item.createdDate).toLocaleString('vi-VN', {
-            hour: '2-digit',
-            minute: '2-digit',
-            day: '2-digit',
-            month: '2-digit',
-            year: 'numeric',
-          }).replace(',', ''),
-        }));
-      } else {
-        console.error('API trả về dữ liệu không hợp lệ:', response);
-        alert('Lỗi khi lấy dữ liệu. Vui lòng thử lại.');
+      // Kiểm tra xem ngày bắt đầu và ngày kết thúc có lớn hơn ngày hiện tại không
+      if (selectedStartDate > today || selectedEndDate > today) {
+        alert('Ngày bắt đầu và ngày kết thúc không được lớn hơn ngày hiện tại.');
+        return;
       }
-    } catch (error) {
-      console.error('Lỗi khi gọi API:', error);
-      alert('Lỗi khi gọi API. Vui lòng thử lại.');
-    }
-  } else if (this.startDate && this.endDate) {
-    // Nếu có cả startDate và endDate thì gọi API với cả hai tham số
-    try {
-      const response = await axios.get(`https://localhost:7162/Request/requests-in-date-range`, {
-        params: {
-          startDate: this.startDate,
-          endDate: this.endDate,
-        },
-      });
 
-      if (response.data && Array.isArray(response.data)) {
-        this.tableData.data = response.data.map((item) => ({
-          requestID: item.requestID,
-          'Mã số phiếu': item.requestCode,
-          'Tổng tiền': new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(item.totalPrice),
-          'Ngày tạo': new Date(item.createdDate).toLocaleString('vi-VN', {
-            hour: '2-digit',
-            minute: '2-digit',
-            day: '2-digit',
-            month: '2-digit',
-            year: 'numeric',
-          }).replace(',', ''),
-        }));
+      // Nếu chỉ có endDate và không có startDate
+      if (!this.startDate && this.endDate) {
+        try {
+          const response = await axios.get(`https://localhost:7162/Request/requests-in-date-range`, {
+            params: {
+              endDate: this.endDate,
+            },
+          });
+
+          const userPromises = response.data.map(item => this.getUserName(item.userID));
+            const userNames = await Promise.all(userPromises);
+
+            if (response.data && Array.isArray(response.data)) {
+                this.tableData.data = response.data
+                .sort((a, b) => new Date(b.createdDate) - new Date(a.createdDate))
+                .map((item, index) => ({ // Thêm index vào map
+                    requestID: item.requestID,
+                    'Mã số phiếu': item.requestCode,
+                    'Người tạo': userNames[index], // Lấy tên người dùng từ mảng đã giải quyết
+              'Ngày tạo': new Date(item.createdDate).toLocaleString('vi-VN', {
+                hour: '2-digit',
+                minute: '2-digit',
+                day: '2-digit',
+                month: '2-digit',
+                year: 'numeric',
+              }).replace(',', ''),
+              'Tổng tiền': new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(item.totalPrice),
+            }));
+          } else {
+            console.error('API trả về dữ liệu không hợp lệ:', response);
+            alert('Lỗi khi lấy dữ liệu. Vui lòng thử lại.');
+          }
+        } catch (error) {
+          console.error('Lỗi khi gọi API:', error);
+          alert('Lỗi khi gọi API. Vui lòng thử lại.');
+        }
+      } else if (this.startDate && this.endDate) {
+        // Nếu có cả startDate và endDate thì gọi API với cả hai tham số
+        try {
+          const response = await axios.get(`https://localhost:7162/Request/requests-in-date-range`, {
+            params: {
+              startDate: this.startDate,
+              endDate: this.endDate,
+            },
+          });
+
+          if (response.data && Array.isArray(response.data)) {
+            this.tableData.data = response.data.map((item) => ({
+              requestID: item.requestID,
+              'Mã số phiếu': item.requestCode,
+              'Tổng tiền': new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(item.totalPrice),
+              'Ngày tạo': new Date(item.createdDate).toLocaleString('vi-VN', {
+                hour: '2-digit',
+                minute: '2-digit',
+                day: '2-digit',
+                month: '2-digit',
+                year: 'numeric',
+              }).replace(',', ''),
+            }));
+          } else {
+            // console.error('API trả về dữ liệu không hợp lệ:', response);
+            alert('Lỗi khi lấy dữ liệu. Vui lòng thử lại.');
+          }
+        } catch (error) {
+          // console.error('Lỗi khi gọi API:', error);
+          alert('Lỗi khi gọi API. Vui lòng thử lại.');
+        }
       } else {
-        // console.error('API trả về dữ liệu không hợp lệ:', response);
-        alert('Lỗi khi lấy dữ liệu. Vui lòng thử lại.');
+        alert('Vui lòng chọn ít nhất ngày kết thúc.');
       }
-    } catch (error) {
-      // console.error('Lỗi khi gọi API:', error);
-      alert('Lỗi khi gọi API. Vui lòng thử lại.');
-    }
-  } else {
-    alert('Vui lòng chọn ít nhất ngày kết thúc.');
-  }
-},
+    },
     async fetchReportDetailData() {
       try {
         const datetimenow = new Date().toISOString().split('T')[0];
