5301c80b9127afe50e3f2f282ca8973986c19b39
DoDucToan <92496468+DoDucToan@users.noreply.github.com>
2025-04-04 11:06:34 +0700

    noti date fix

--- vue-light-bootstrap-dashboard-master (1)/vue-light-bootstrap-dashboard-master/src/pages/Notifications.vue
+++ vue-light-bootstrap-dashboard-master (1)/vue-light-bootstrap-dashboard-master/src/pages/Notifications.vue
@@ -189,8 +189,14 @@ export default {
       }
     },
     formatDate(date) {
-      const options = { year: 'numeric', month: 'long', day: 'numeric' };
-      return new Date(date).toLocaleDateString(undefined, options);
+      const options = {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+      };
+      return new Date(date).toLocaleString('vi-VN', options).replace(',', '');
     },
   },
   async mounted() {
